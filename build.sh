13fafe6b8bc930dcf55c33cd20ff6778fbcb7d68
Matthieu Cattin <matthieu.cattin@cern.ch>
2013-03-20 09:19:59 +0100

    Remove build script. Not generic enough.

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Generate Python file for GUI layout."
-pyuic4 -xo tcxgen_gui_layout.py tcxgen_gui_layout.ui
-
-echo "Generate binary from Python."
-python ~/pyinstaller-2.0/pyinstaller.py tcxgen.spec
